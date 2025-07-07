c880693abee2c5c70b39ed6d511dbe3c6b29db80
dmitryreaper <dimadimof81@gmail.com>
2025-07-07 20:58:13 +0300

    Combining all modules

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+
+	"GNote/storage"
+	"GNote/ui"
+)
+
+func main() {
+	// Загрузка конфигурации БД из переменных окружения
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		dbHost = "localhost"
+	}
+	dbPortStr := os.Getenv("DB_PORT")
+	dbPort, err := strconv.Atoi(dbPortStr)
+	if err != nil {
+		dbPort = 5432 
+}
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == "" {
+		dbUser = "dima" 
+	}
+	dbPassword := os.Getenv("DB_PASSWORD")
+	if dbPassword == "" {
+		dbPassword = "" 
+	}
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		dbName = "notes_db"
+	}
+	dbSSLMode := os.Getenv("DB_SSLMODE")
+	if dbSSLMode == "" {
+		dbSSLMode = "disable" // 'disable' для локальной разработки
+	}
+
+	dbConfig := storage.Config{
+		Host:     dbHost,
+		Port:     dbPort,
+		User:     dbUser,
+		Password: dbPassword,
+		DBName:   dbName,
+		SSLMode:  dbSSLMode,
+	}
+
+	// Инициализация хранилища (PostgreSQL)
+	store, err := storage.NewPostgresStore(dbConfig)
+	if err != nil {
+		log.Fatalf("Ошибка при инициализации хранилища БД: %v", err)
+	}
+
+	// Инициализация Fyne приложения
+	a := app.New()
+	w := a.NewWindow("Приложение для заметок")
+	w.SetIcon(fyne.NewStaticResource("note.ico", []byte{})) 
+
+	// Создание и запуск UI приложения
+	noteApp := ui.NewNoteApp(w, store)
+	_ = noteApp 
+	w.ShowAndRun()
+}
