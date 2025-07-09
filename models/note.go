83d43fc0fc0b08a492abe4a37dcab59edd489518
dmitryreaper <dimadimof81@gmail.com>
2025-07-09 21:46:42 +0300

    add: new struct attachment

--- models/note.go
+++ models/note.go
@@ -4,13 +4,24 @@ import (
 	"time"
 )
 
-// Note представляет собой структуру заметки
 type Note struct {
 	ID         int        `json:"id"`
 	Title      string     `json:"title"`
 	Content    string     `json:"content"`
-	Tags       []string   `json:"tags"` 
-	ReminderAt *time.Time `json:"reminder_at"`
 	CreatedAt  time.Time  `json:"created_at"`
 	UpdatedAt  time.Time  `json:"updated_at"`
+	ReminderAt *time.Time `json:"reminder_at"`
+	Tags       []string   `json:"tags"`
+	Attachments []Attachment `json:"attachments"` 
+}
+
+// структура вложения
+type Attachment struct {
+	ID        int        `json:"id"`
+	NoteID    int        `json:"note_id"`
+	Filename  string     `json:"filename"`
+	Filepath  string     `json:"filepath"` // путь на диске
+	MimeType  string     `json:"mime_type"`
+	SizeBytes int64      `json:"size_bytes"`
+	UploadedAt time.Time `json:"uploaded_at"`
 }
